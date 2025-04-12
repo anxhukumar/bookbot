fe5cfda64649fa0172d6984c75ee7dbece7231b4
anxhukumar <contact.anshukumar@protonmail.com>
2025-04-12 15:29:47 +0530

    complete bookbot cli tool

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sorted_list_of_dictionary
+import sys
 
 def main():
-  book_content = get_book_text( "./books/frankenstein.txt" )
+
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+  book_content = get_book_text( f"{sys.argv[1]}" )
   num_of_words = count_words( book_content )
-  print(f"{num_of_words} words found in the document")
-  num_of_characters = count_characters( book_content )
-  print(num_of_characters)
+  unsorted_dict = count_characters( book_content )
+  sorted_list_of_dict = sorted_list_of_dictionary( unsorted_dict )
+  
+  print( "============ BOOKBOT ============" )
+  print( f"Analyzing book found at {sys.argv[1]}..." )
+  print( "----------- Word Count ----------" )
+  print( f"Found {num_of_words} total words " )
+  print( "--------- Character Count -------" )
+  
+  for item in sorted_list_of_dict:
+    if (item[ "character" ].isalpha()):
+      print( f"{ item[ "character" ] }: { item[ "count" ] }" )
 
 def get_book_text( file_path ):
   with open( file_path ) as f:
--- stats.py
+++ stats.py
@@ -1,14 +1,34 @@
 def count_words( book_content ):
   words_list = book_content.split()
   number_of_words = len( words_list )
+  
   return number_of_words
 
 def count_characters( book_content ):
   counter = {}
+  
   for character in book_content:
       character = character.lower()
-      if (character in counter):
-        counter[character] +=1
+      if character in counter:
+        counter[ character ] +=1
       else:
-        counter[character] = 1
+        counter[ character ] = 1
+  
   return counter
+
+def sorted_list_of_dictionary( dict ):
+  sorted_counter_dict_list = []
+  
+  for character in dict:
+    new_dict = {
+      'character': character,
+      'count': dict[ character ]
+    }
+    sorted_counter_dict_list.append( new_dict )
+  
+  def sort_on( dictionary ):
+    return dictionary[ "count" ]
+  
+  sorted_counter_dict_list.sort( reverse=True, key=sort_on )
+  
+  return sorted_counter_dict_list
